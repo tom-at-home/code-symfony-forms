d41fff91862b7f9aeb16ceba18855e7bb9c24278
Tomasz <t.urbaniak@pj-mbp.local>
2020-01-09 16:54:07 +0100

    initial commit

---
+++ start/src/Controller/ArticleAdminController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleFormType;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleAdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/article/new", name="admin_article_new")
+     * @IsGranted("ROLE_ADMIN_ARTICLE")
+     */
+    public function new(EntityManagerInterface $em, Request $request)
+    {
+        $form = $this->createForm(ArticleFormType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            /** @var Article $data */
+            $article = $form->getData();
+            $article->setAuthor($this->getUser());
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Your Article was created!');
+
+            return $this->redirectToRoute('admin_article_list');
+        }
+
+
+        return $this->render('article_admin/new.html.twig', [
+            'articleForm' => $form->createView(),
+        ]);
+
+    }
+
+    /**
+     * @Route("/admin/article/{id}/edit")
+     * @IsGranted("MANAGE", subject="article")
+     */
+    public function edit(Article $article)
+    {
+        dd($article);
+    }
+
+    /**
+     * @Route("/admin/article", name="admin_article_list")
+     */
+    public function list(ArticleRepository $articleRepository)
+    {
+        $articles = $articleRepository->findAll();
+
+        return $this->render('article_admin/list.html.twig', [
+            'articles' => $articles
+        ]);
+
+    }
+}
