83c76b1ea0235621a3972c438c641d382fea10b7
Tomasz <t.urbaniak@pj-mbp.local>
2020-01-13 15:26:25 +0100

    Chapter 35

--- start/src/Form/ArticleFormType.php
+++ start/src/Form/ArticleFormType.php
@@ -9,6 +9,7 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -38,6 +39,22 @@ class ArticleFormType extends AbstractType
             ])
             ->add('author', UserSelectTextType::class, [
                 'disabled' => $isEdit
+            ])
+            ->add('location', ChoiceType::class, [
+                'placeholder' => 'Choose a localtion',
+                'choices' => [
+                    'The Solar System' => 'solar_system',
+                    'Near a star' => 'star',
+                    'Interstellar Space' => 'interstellar_space'
+                ],
+                'required' => false
+            ])
+            ->add('specificLocationName', ChoiceType::class, [
+                'placeholder' => 'Where exactly?',
+                'choices' => [
+                    'TODO' => 'TODO'
+                ],
+                'required' => false
             ]);
 
         if ($options['include_published_at']) {
